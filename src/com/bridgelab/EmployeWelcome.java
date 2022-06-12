63fa1e3139b317a0af73e2c6ab86dc6d6aa4afcd
Shashank10101010 <shashankpraveen23@gmail.com>
2022-06-12 14:00:25 +0530

    Welcome

---
+++ src/com/bridgelab/EmployeWelcome.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class EmployeWelcome {
+    public static void main (String [] args){
+        System.out.println("Welcome to employee wage computation program");
+    }
+}
