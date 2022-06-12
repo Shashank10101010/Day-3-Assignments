633bf7ca83ee64d1abf9ec1eafbcaba8412766d0
Shashank10101010 <shashankpraveen23@gmail.com>
2022-06-12 14:42:38 +0530

    UC2

---
+++ src/com/bridgelab/UC2DailyEmployeeWage.java
@@ -0,0 +1,20 @@
+package com.bridgelab;
+
+public class UC2DailyEmployeeWage {
+    public static void main(String[] args) {
+        int isFullTime = 1;
+        int wagePerHour = 20;
+        int employeeHours;
+        int employeeWage;
+
+        double employeePresence = Math.floor(Math.random() * 10) % 2;
+        if (employeePresence == isFullTime) {
+            employeeHours = 8;
+            employeeWage = employeeHours * wagePerHour;
+            System.out.println("Wage of the employee is " + employeeWage + " Rs");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
+    }
+}
